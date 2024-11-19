25655487092661921593b19a27ee10e7e38786d6
azza-mohamed30 <azzamohamed5it@gmail.com>
2024-11-20 00:06:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 //         img.src = "assets/images/placeholder.png"; // Replace with your image path when ready
 //     });
 // });
+
+
+// start statistics-section code js***************
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+
+// end statistics-section code js ***************
